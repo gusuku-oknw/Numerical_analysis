303488f3b78bb45361b445c55fab9e00549016cb
tmkjn <tmkjny@gmail.com>
2023-05-25 21:55:25 +0900

    2023/05/25

--- 164.sor.plt.py
+++ 164.sor.plt.py
@@ -1,4 +1,16 @@
-import draw
+import matplotlib
+import matplotlib.pyplot as plot
+
+
+# 描画
+def draw(x, y):
+    fig = plot.figure()
+    axe1 = fig.add_subplot(2, 1, 1)
+    axe2 = fig.add_subplot(2, 1, 2)
+    axe1.plot(x, y)
+    axe2.plot(x, color="red", marker="o")
+    axe2.plot(y, color="blue", marker="o")
+    plot.show()
 
 
 def progress(k, x):
