9143bfa135e9a5b7ad35d91672b2eea63969329e
タマキジュンヤ <tmkjny@gmail.com>
2023-05-25 11:32:34 +0900

    2023/05/25

--- draw.py
+++ draw.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plot
 # 描画
 def draw(x, y):
     fig = plot.figure()
-    plot.plot(x, color="red", marker="o")
-    plot.plot(y, color="blue", marker="o")
-    # plot.scatter(x, y, s=30, c="red")
-    # plot.scatter(y, x, s=30, c="blue")
+    axe1 = fig.add_subplot(2, 1, 1)
+    axe2 = fig.add_subplot(2, 1, 2)
+    axe1.plot(x, y)
+    axe2.plot(x, color="red", marker="o")
+    axe2.plot(y, color="blue", marker="o")
     plot.show()
+
 
 x = [0, 1, 2, 3]
 y = [3, 2, 1, 0]
